0fb9d524e6630c05ecc18daea82b8c4179361b63
jaforrockman <jaforrockman695@gmail.com>
2015-05-13 21:37:22 +0600

    edited customer_id to name

--- db/migrate/20150513130419_remove_customer_name_from_bills.rb
+++ db/migrate/20150513130419_remove_customer_name_from_bills.rb
@@ -1,5 +1,5 @@
 class RemoveCustomerNameFromBills < ActiveRecord::Migration
   def change
-    remove_column :bills, :customer_id, :string
+    remove_column :bills, :customer_name, :string
   end
 end
