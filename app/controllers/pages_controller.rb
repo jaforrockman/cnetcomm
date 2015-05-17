c5168ffeb81a02c82344740efdd99414ca5714e8
jaforrockman <jaforrockman695@gmail.com>
2015-05-17 13:42:01 +0600

    Added monthly view to bi

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,8 @@ def admin_panel
 end
 
   def bill_list
-  	@bills = Bill.all
+  	@bills = Bill.all.order("created_at DESC")
+    @bill_by_months = @bills.group_by{|bill| bill.created_at.strftime("%B %Y")}
   end
 
 
